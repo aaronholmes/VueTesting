0a596d434725ee1a19b2884af72d8ba02964993d
Aaron Holmes <aholmes@pureguru-software.com>
2018-05-09 16:37:31 -0400

    Adding tests

--- test/reminders.spec.js
+++ test/reminders.spec.js
@@ -1,11 +1,22 @@
 import { mount } from 'vue-test-utils';
-import { expect } from 'expect';
+import expect  from 'expect';
 import Reminders from '../src/components/Reminders.vue';
 
 describe ('Reminders', () => {
     it ('hides the reminders list if there are none', () => {
         let wrapper = mount(Reminders);
-
         expect(wrapper.contains('ul')).toBe(false);
     });
+
+    it ('can add items', () =>{
+        let wrapper = mount(Reminders);
+        let newReminder = wrapper.find('.new-reminder');
+
+        newReminder.element.value = "Go to the store";
+        newReminder.trigger('input');
+
+        wrapper.find('button').trigger('click');
+
+        expect(wrapper.find('ul').text()).toContain('Go to the store');
+    })
 })
