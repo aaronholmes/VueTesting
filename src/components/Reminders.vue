6b1c1b8bfbeb376dd9db042f48d5e9757ac28afb
Aaron Holmes <aholmes@pureguru-software.com>
2018-05-09 17:27:44 -0400

    Getting tests for reminders to pass

--- src/components/Reminders.vue
+++ src/components/Reminders.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
+        <input class="new-reminder" v-model="newReminder">
+        <button @click="addReminder">Add Reminder</button>
 
+        <ul v-if="reminders.length">
+            <li v-for="reminder in reminders" v-text="reminder"></li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-
+        data () {
+            return {
+                newReminder: "",
+                reminders: []
+            }
+        },
+        methods: {
+            addReminder() {
+                this.reminders.push(this.newReminder);
+                this.newReminder = '';
+            }
+        }
     }
 </script>
