f60377fb0e6f14cc4eb69c59b5fa731fc1ba80f0
Aaron Holmes <aholmes@pureguru-software.com>
2018-05-09 16:27:14 -0400

    Fleshing out test for TDD

---
+++ test/reminders.spec.js
@@ -0,0 +1,11 @@
+import { mount } from 'vue-test-utils';
+import { expect } from 'expect';
+import Reminders from '../src/components/Reminders.vue';
+
+describe ('Reminders', () => {
+    it ('hides the reminders list if there are none', () => {
+        let wrapper = mount(Reminders);
+
+        expect(wrapper.contains('ul')).toBe(false);
+    });
+})
